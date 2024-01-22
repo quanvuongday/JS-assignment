1fe31775f64782f84bacba39bfcf19ad2a79c08f
quanvuongday <quanthuy21299@gmail.com>
2024-01-22 13:13:06 +0200

    strings02

---
+++ strings_02.js
@@ -0,0 +1,19 @@
+/**
+Refine the shoutMyName function to return the name parameter it receives, converted entirely to uppercase.
+ */
+
+function shoutMyName(name) {}
+
+// Sample usage - do not modify
+console.log(shoutMyName('Sam')); // "SAM"
+console.log(shoutMyName('Charley')); // "CHARLEY"
+console.log(shoutMyName('alex')); // "ALEX"
+
+let name = "Sam".toUpperCase();
+console.log(name);
+
+let first = "Charley".toUpperCase();
+console.log(first);
+
+let last = "alex".toUpperCase();
+console.log(last);
