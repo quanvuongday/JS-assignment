0760c2981a7b9a709e49bbe2b2c2f3110da86e8a
quanvuongday <quanthuy21299@gmail.com>
2024-01-22 13:02:44 +0200

    strings01

---
+++ basic_01.js
@@ -0,0 +1,9 @@
+// Write the function sum such that it returns the sum of a and b.
+
+// sample usage - do not modify
+
+let a = 1, b = 3;
+function sum (a,b ) {
+    return a + b; 
+}
+console.log(sum);
