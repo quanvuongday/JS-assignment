4da93b03e4a46432b1314c3ba57b1e7a3642b76f
quanvuongday <quanthuy21299@gmail.com>
2024-01-22 14:29:35 +0200

    strings04

---
+++ strings_04.js
@@ -0,0 +1,15 @@
+/**
+Refine the getFirstCharacter function to return the first character of the name parameter provided to it.
+ */
+
+function getFirstCharacter(name) {}
+
+// Sample usage - do not modify
+console.log(getFirstCharacter('Amsterdam')); // "A"
+console.log(getFirstCharacter('Japan')); // "J"
+
+let city = 'Amsterdam';
+console.log(city[0]);
+
+let country = 'Japan';
+console.log(country[0]);
