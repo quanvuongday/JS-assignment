b49da950bf4098c8ca538bb06a6af2811f12579b
quanvuongday <quanthuy21299@gmail.com>
2024-01-22 13:34:54 +0200

    strings03

---
+++ strings_03.js
@@ -0,0 +1,15 @@
+/**
+Refine the lowerName function to return the name parameter it receives, entirely in lowercase.
+
+ */
+function lowerName(name) {}
+
+// Sample usage - do not modify
+console.log(lowerName('Sam')); // "sam"
+console.log(lowerName('ALEX')); // "alex"
+
+let name = "Sam".toLowerCase();
+console.log(name);
+
+let first = "ALEX".toLowerCase();
+console.log(first);
